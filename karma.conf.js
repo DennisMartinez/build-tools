81cab612c0d7e52b575692065b41d06519abb1b0
jfusco <jfusco@wreckingballmedia.com>
2017-04-01 11:52:42 -0400

    Add initial files for unit testing
    Add initial karma.confg file with settings and proper loaders.
    Add karma and other dependencies to package.json
    Update some outdated libraries in package.json

---
+++ karma.conf.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var webpack = require('karma-webpack');
+var path = require('path');
+
+module.exports = function (config) {
+	config.set({
+		frameworks: ['jasmine'],
+		files: ['__tests__/*-test.js'],
+		plugins: [webpack, 'karma-jasmine', 'karma-phantomjs-launcher', 'karma-spec-reporter', 'karma-coverage'],
+		browsers: ['PhantomJS'],
+		preprocessors: {
+			'__tests__/*-test.js': ['webpack']
+		},
+		coverageReporter: {
+			dir: './coverage',
+			reporters: [
+				{ type: 'lcov', subdir: 'reports' }
+			]
+		},
+		colors: true,
+		reporters: ['spec', 'coverage'],
+		webpack: {
+			module: {
+				rules: [
+					{
+						test: /\.js?$/,
+						loader: 'babel-loader',
+						exclude: /(node_modules)/
+					},
+					{
+						enforce: 'pre',
+						test: /\.js/,
+						loader: 'isparta-loader',
+						exclude: /(__tests__|node_modules)/
+					}
+				],
+				noParse: [
+					/node_modules\/sinon\//
+				]
+			},
+			resolve: {
+				alias: {
+					'sinon': 'sinon/pkg/sinon.js'
+				}
+			}
+		},
+		webpackMiddleware: { noInfo: true }
+	});
+};
